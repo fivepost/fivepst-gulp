ca5a88f61cc021b922baaa5bff42f271b1435a32
fivepost <fivepost@yandex.ru>
2021-07-22 14:09:37 +0500

    Clean build folder task and CSS task added.

--- gulpfile.js
+++ gulpfile.js
@@ -29,7 +29,15 @@ const path = {
 
 const { src, dest, series, parallel } = require('gulp'),
 	browsersync = require('browser-sync').create(),
-	fileinclude = require('gulp-file-include')
+	fileinclude = require('gulp-file-include'),
+	del = require('del'),
+	scss = require('gulp-sass')(require('node-sass')),
+	clean_css = require('gulp-clean-css'),
+	autoprefixer = require('gulp-autoprefixer'),
+	group_media = require('gulp-group-css-media-queries'),
+	prefix = require('gulp-rename'),
+	strip_comments = require('gulp-strip-css-comments');
+
 
 
 
@@ -50,8 +58,35 @@ function html(params) {
 		.pipe(browsersync.stream())
 }
 
+function css(params) {
+	return src(path.app.css)
+		.pipe(scss({
+			outputStyle: 'expanded',
+			includePaths: ['node_modules']
+		}))
+		.pipe(group_media())
+		.pipe(autoprefixer({
+			overrideBrowserslist: ['last 10 versions'],
+			cascade: true
+		}))
+		.pipe(strip_comments())
+		.pipe(dest(path.build.css))
+		.pipe(clean_css())
+		.pipe(prefix({
+			extname: '.min.css'
+		}))
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream())
+}
 
-const watch = parallel(browserSync)
+function clean(params) {
+	return del(path.clean)
+}
+
+
+
+
+const watch = series(clean, html, css, browserSync)
 
 exports.watch = watch
 exports.default = watch
